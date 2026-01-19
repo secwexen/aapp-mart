c7c48ab48d7cc79b9b76e4c7b29978ec7b3cab67
The Secwexen <129677392+secwexen@users.noreply.github.com>
2026-01-19 16:17:51 +0300

    Update utils.py
    Signed-off-by: The Secwexen <129677392+secwexen@users.noreply.github.com>

--- aappmart/core/utils.py
+++ aappmart/core/utils.py
@@ -1,1 +1,10 @@
+import logging
 
+def log_info(msg):
+    logging.info(f"[UTIL] {msg}")
+
+def log_warning(msg):
+    logging.warning(f"[UTIL] {msg}")
+
+def log_error(msg):
+    logging.error(f"[UTIL] {msg}")

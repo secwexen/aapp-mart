6e5e9279d6f6992ce98e318f13fc400290c2e813
The Secwexen <129677392+secwexen@users.noreply.github.com>
2026-01-21 11:15:56 +0300

    Update simulation_engine.py
    Signed-off-by: The Secwexen <129677392+secwexen@users.noreply.github.com>

--- aappmart/core/simulation_engine.py
+++ aappmart/core/simulation_engine.py
@@ -1,14 +1,1 @@
-import logging
-from core.pipeline import Pipeline
-from agents.agent_registry import AgentRegistry
 
-class SimulationEngine:
-
-    def __init__(self):
-        self.agents = AgentRegistry.get_all_agents()
-        self.pipeline = Pipeline(steps=5)
-
-    def run(self):
-        logging.info("Simulation Engine started")
-        self.pipeline.execute(self.agents)
-        logging.info("Simulation Engine finished")

3bdfe416f1ca659be9e946c48aa9cffa731f57c1
The Secwexen <129677392+secwexen@users.noreply.github.com>
2026-07-23 09:40:28 +0300

    Update advanced_attack_simulation.py

--- demo/advanced_attack_simulation.py
+++ demo/advanced_attack_simulation.py
@@ -169,7 +169,7 @@ def main():
         print(f" [!] {asset}")
 
     output_file = (
-        f"/var/log/aapp-mart/logs/attack-path/"
+        f"./aapp-mart/logs/attack-path/"
         f"attack_path_report.json"
     )
 
